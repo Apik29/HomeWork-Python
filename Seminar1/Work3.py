5aed79d1b2beaaf43327d680c3f72431bd9d3d24
Apik29 <121308884+Apik29@users.noreply.github.com>
2023-03-14 15:46:25 +0300

    easy

--- Seminar1/Work3.py
+++ Seminar1/Work3.py
@@ -3,11 +3,19 @@
 Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета."""
 
 num = int(input("Введи шестизначный номер Вашего билета:"))
-if (len(num) != 6):
+if num < 100000 or num > 999999:
     print("Вы ввесли не шестизначный номер билета")
+
 else:
-    sum1 = int(num[0]) + int(num[1]) + int(num[2])
-    sum2 = int(num[3]) + int(num[4]) + int(num[5])
+    num1 = int(num/100000)
+    num2 = int(num/10000) % 10
+    num3 = int(num/1000) % 10
+    num4 = int(num/100) % 10
+    num5 = int(num/10) % 10
+    num6 = num % 10
+
+    sum1 = num1 + num2 + num3
+    sum2 = num4 + num5 + num6
     if sum1 == sum2:
         print("Ваш билет счастливый :)")
     else:
